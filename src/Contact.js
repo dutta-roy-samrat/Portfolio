0abf95751162b773ebccca919cf4aa228beb8881
Zer0 <samratduttaroy008@gmail.com>
2022-03-24 15:38:36 +0530

    Contact and footer added

---
+++ src/Contact.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Contact(){
+    return(
+    <div>
+        <div className="phone_num">
+            <h5>Call</h5>
+            <p>
+                +91-7002919673
+            </p>
+        </div>
+        <div className="email">
+            <h5>Write</h5>
+            <a href="mailto:samratduttaroy008@gmail.com">samratduttaroy008@gmail.com</a>
+        </div>
+        <div className="socials">
+            <h5>Follow</h5>
+            <div>
+                <a href=""><i className="fa-brands fa-facebook-f"></i></a>
+                <a href=""><i className="fa-brands fa-linkedin-in"></i></a>
+                <a href=""><i className="fa-brands fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+    )
+}
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+    <footer>
+        &copy; 2022 by Samrat Dutta Roy.
+    </footer>
+    )
+}
