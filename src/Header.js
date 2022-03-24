ddcb74fca9f4555837ba8c45a0e0ed5862ba34af
Zer0 <samratduttaroy008@gmail.com>
2022-03-24 13:50:06 +0530

    Header Added

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+    <header className="App-header">
+        <h1>Samrat Dutta Roy</h1>
+        <h4>SENIOR ANALYST</h4>
+    </header>)
+}
