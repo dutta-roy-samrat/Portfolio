6a61947a0ee03eb2f6db9fa1d0c51e4d4c589787
Zer0 <samratduttaroy008@gmail.com>
2022-03-24 20:41:43 +0530

    Majorly Socials Path added

--- src/Socials.js
+++ src/Socials.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Socials(){
     return(
     <div>
-        <a href=""><i className="fa-brands fa-facebook-f"></i></a>
-        <a href=""><i className="fa-brands fa-linkedin-in"></i></a>
-        <a href=""><i className="fa-brands fa-instagram"></i></a>
+        <a href="https://www.facebook.com/profile.php?id=100007717817575" target="_blank"><i className="fa-brands fa-facebook-f"></i></a>
+        <a href="https://www.linkedin.com/in/samrat-dutta-roy-032a86194/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
+        <a href="https://www.instagram.com/cheerful_lad/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
     </div>
     )
 }
