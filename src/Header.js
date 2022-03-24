cf53858839410079a26f32ce98acf5f3b757c119
Zer0 <samratduttaroy008@gmail.com>
2022-03-24 15:57:07 +0530

    Added paths to socials

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,6 @@ export default function Header(){
     return(
     <header className="App-header">
         <h1>Samrat Dutta Roy</h1>
-        <h4>SENIOR ANALYST</h4>
+        <h4><span className='green_dot'></span>SENIOR ANALYST</h4>
     </header>)
 }
