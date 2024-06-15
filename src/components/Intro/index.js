523640ea90c05b4e1bd8d775ff949d0ac94f7930
Samrat Dutta Roy <samratduttaroy008@gmail.com>
2024-06-15 20:17:25 +0530

    Portfolio content updated

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from './styles.module.css'
-export default function Intro(){
-    return(
+import styles from "./styles.module.css";
+export default function Intro() {
+  return (
     <div className={`${styles.intro}`}>
-        <p className={styles.greetings}>Hello</p>
-        <h3 className={styles.starting_intro}>Here's who I am AND what I do</h3>
-        <div className={styles.btn}>
-                <NavLink to='/resume'><button className={styles.resume_btn}>RESUME</button></NavLink>
-                <NavLink to='/projects'><button className={styles.projects_btn}>PROJECTS</button></NavLink>
-        </div>
-        <p className={styles.about_myself}>
-            I am a web development enthusiast. I am on a journey to learn and polish my frontend skills. I have acquired skills in HTML, CSS, React and JavaScript .
-        </p>
-        <p className={styles.about_myself}>
-            I am actually a Mechnaical Engineer by degree. But the addictive nature of coding got me and here I am developing and learning frontend of web to create some efficient and functional interfaces.
-        </p>
+      <p className={styles.greetings}>Hello</p>
+      <h3 className={styles.starting_intro}>Here's who I am AND what I do</h3>
+      <div className={styles.btn}>
+        <NavLink to="/resume">
+          <button className={styles.resume_btn}>RESUME</button>
+        </NavLink>
+        <NavLink to="/projects">
+          <button className={styles.projects_btn}>PROJECTS</button>
+        </NavLink>
+      </div>
+      <p className={styles.about_myself}>
+        I am a dedicated web development enthusiast actively honing my frontend
+        skills. I specialize in React, JavaScript, Next.js, and various other
+        essential frontend tools, continually striving to expand my knowledge
+        and expertise.
+      </p>
+      <p className={styles.about_myself}>
+        Originally a Mechanical Engineer, I've transitioned into a passionate
+        front-end web developer, dedicated to crafting efficient and engaging
+        user interfaces.
+      </p>
     </div>
-    )
+  );
 }
--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,70 +1,134 @@
 import React from "react";
-import Card from '../../components/Card'
-import styles from './styles.module.css'
-import CV from '../../Samrat_Resume.pdf'
-export default function Resume(){
-    React.useEffect(()=>window.scrollTo(0,0),[])
-    return(
-        <div className={styles.resume_section}>
-            <h1 className={styles.resume}><span className={styles.blue_dot}> </span>Resume</h1>
-            
-            <div className={styles.exp_section}>
-            <div className={styles.resume_access}>
-                <h3 className={styles.exp}>Experience</h3>
-                <a href={CV} target='_blank'><button className={styles.cv}>DOWNLOAD CV</button></a>
-            </div>
-            <Card render={()=>(
-                <>
-                    <p className={styles.date}>2021-Present</p>
-                    <p className={styles.job_pos}>SENIOR ANALYST</p>
-                    <p className={styles.company}>Capgemini India Pvt. Ltd.</p>
-                    <p className={styles.location}>Bangalore</p>
-                </>
-            )} className={styles.card}
-            />
-            </div>
-            <div className={styles.edu_section}>   
-                <h3 className={styles.edu}>Education</h3>
-                <Card render={()=>(
-                    
-                <>
-                        <p className={styles.date}>2017-2021</p>
-                        <p className={styles.institute}>NATIONAL INSTITUTE OF TECHNOLOGY SILCHAR</p>
-                        <p className={styles.degree}>Bachelor Of Technolgy</p>
-                        <p className={styles.location}>Silchar,Assam</p>
-        
-                        <p className={styles.date}>2014-2016</p>
-                        <p className={styles.institute}>RAMANUJ GUPTA JUNIOR COLLEGE</p>
-                        <p className={styles.degree}>Senior Secondary</p>
-                        <p className={styles.location}>Silchar,Assam</p>
-                        
-                        <p className={styles.date}>2002-2014</p>
-                        <p className={styles.institute}>ST. MARY'S SCHOOL</p>
-                        <p className={styles.degree}>Secondary</p>
-                        <p className={styles.location}>Hailakandi,Assam</p>
-                        
-                    </>
-                    
-                )} className={styles.card}
-                />
-            </div>
-            <div className={styles.skills}>
-                <Card render={()=>(
-                    <>
-                        <p className={styles.prof_header}>Professional Skillset</p>
-                        <p className={styles.prof}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>React</p>
-                        <p className={styles.prof}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>HTML</p>
-                        <p className={styles.prof}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>CSS</p>
-                        <p className={styles.prof}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>C++</p>
-                        <p className={styles.prof}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>OOPs</p>
-                        <p className={styles.lang_header}>Languages</p>
-                        <p className={styles.lang}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>English (proficient)</p>
-                        <p className={styles.lang}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}> </span>Bengali (native)</p>
-                        <p className={styles.lang}><span className={styles.blue_dot} style={{width:'13px',height:'13px'}}></span>Hindi (proficient)</p>
-                    </>
-                )} className={styles.skillsCard}
-                />
-            </div>
+import Card from "../../components/Card";
+import styles from "./styles.module.css";
+import CV from "../../Samrat_Resume.pdf";
+export default function Resume() {
+  React.useEffect(() => window.scrollTo(0, 0), []);
+  return (
+    <div className={styles.resume_section}>
+      <h1 className={styles.resume}>
+        <span className={styles.blue_dot}> </span>Resume
+      </h1>
+
+      <div className={styles.exp_section}>
+        <div className={styles.resume_access}>
+          <h3 className={styles.exp}>Experience</h3>
+          <a href={CV} target="_blank">
+            <button className={styles.cv}>DOWNLOAD CV</button>
+          </a>
         </div>
-    )
+        <Card
+          render={() => (
+            <>
+              <p className={styles.date}>2022-Present</p>
+              <p className={styles.job_pos}>FRONTEND DEVELOPER</p>
+              <p className={styles.company}>Vinsol|STADIUM.</p>
+              <p className={styles.location}>Remote</p>
+            </>
+          )}
+          className={styles.card}
+        />
+        <Card
+          render={() => (
+            <>
+              <p className={styles.date}>2021-2022</p>
+              <p className={styles.job_pos}>SENIOR ANALYST</p>
+              <p className={styles.company}>Capgemini India Pvt. Ltd.</p>
+              <p className={styles.location}>Remote</p>
+            </>
+          )}
+          className={styles.card}
+        />
+      </div>
+      <div className={styles.edu_section}>
+        <h3 className={styles.edu}>Education</h3>
+        <Card
+          render={() => (
+            <>
+              <p className={styles.date}>2017-2021</p>
+              <p className={styles.institute}>
+                NATIONAL INSTITUTE OF TECHNOLOGY SILCHAR
+              </p>
+              <p className={styles.degree}>Bachelor Of Technolgy</p>
+              <p className={styles.location}>Silchar,Assam</p>
+
+              <p className={styles.date}>2014-2016</p>
+              <p className={styles.institute}>RAMANUJ GUPTA JUNIOR COLLEGE</p>
+              <p className={styles.degree}>Senior Secondary</p>
+              <p className={styles.location}>Silchar,Assam</p>
+
+              <p className={styles.date}>2002-2014</p>
+              <p className={styles.institute}>ST. MARY'S SCHOOL</p>
+              <p className={styles.degree}>Secondary</p>
+              <p className={styles.location}>Hailakandi,Assam</p>
+            </>
+          )}
+          className={styles.card}
+        />
+      </div>
+      <div className={styles.skills}>
+        <Card
+          render={() => (
+            <>
+              <p className={styles.prof_header}>Professional Skillset</p>
+              <p className={styles.prof}>
+                <span
+                  className={styles.blue_dot}
+                  style={{ width: "13px", height: "13px" }}
+                >
+                  {" "}
+                </span>
+                React
+              </p>
+              <p className={styles.prof}>
+                <span
+                  className={styles.blue_dot}
+                  style={{ width: "13px", height: "13px" }}
+                >
+                  {" "}
+                </span>
+                JavaScript
+              </p>
+              <p className={styles.prof}>
+                <span
+                  className={styles.blue_dot}
+                  style={{ width: "13px", height: "13px" }}
+                >
+                  {" "}
+                </span>
+                Next.js
+              </p>
+              <p className={styles.lang_header}>Languages</p>
+              <p className={styles.lang}>
+                <span
+                  className={styles.blue_dot}
+                  style={{ width: "13px", height: "13px" }}
+                >
+                  {" "}
+                </span>
+                English (proficient)
+              </p>
+              <p className={styles.lang}>
+                <span
+                  className={styles.blue_dot}
+                  style={{ width: "13px", height: "13px" }}
+                >
+                  {" "}
+                </span>
+                Bengali (native)
+              </p>
+              <p className={styles.lang}>
+                <span
+                  className={styles.blue_dot}
+                  style={{ width: "13px", height: "13px" }}
+                ></span>
+                Hindi (proficient)
+              </p>
+            </>
+          )}
+          className={styles.skillsCard}
+        />
+      </div>
+    </div>
+  );
 }
